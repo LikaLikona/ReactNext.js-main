9910ba6fc084574531050589c99e66928b2a8b6f
LikaLikona <likalikona@gmail.com>
2024-04-25 19:43:17 +0400

    1

---
+++ app/Signup/page.js
@@ -0,0 +1,201 @@
+'use client'
+import { useState } from 'react';
+import Image from 'next/image'
+
+export default function Signup() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleToggle = () => {
+        // Use the functional update form to toggle the isChecked state
+        setIsChecked(prevState => !prevState);
+    };
+
+  return (
+    <main className="flex flex-col md:flex-row min-h-screen">
+      {/* Left Section */}
+      <section className="flex-1 flex flex-col items-center justify-center">
+        {/* Logo */}
+        <div className="absolute top-4 left-4">
+          <Image
+            src=" " 
+            alt="Logo" 
+            width={100} 
+            height={100} 
+          />
+        </div>
+        
+        <h1 className="text-6xl text-white font-bold transition-transform transform hover:scale-150 duration-1000 ease-in-out">
+          Weather Today
+        </h1>
+      </section>
+
+      {/* Right Section */}
+      <section className="flex-1 flex items-center justify-center md:item-start overflow-y-auto p-20">
+        <div className="p-6 md:p-12 rounded-lg shadow-xl w-full max-w-lg bg-white ">
+          
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign up for an account
+              </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
+                    First Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="firstName"
+                      name="firstName"
+                      type="text"
+                      autoComplete="given-name"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
+                    Last Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="lastName"
+                      name="lastName"
+                      type="text"
+                      autoComplete="family-name"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="new-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">
+                    Confirm Password
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      type="password"
+                      autoComplete="new-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="dob" className="block text-sm font-medium leading-6 text-gray-900">
+                    Date of Birth
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="dob"
+                      name="dob"
+                      type="date"
+                      autoComplete="bday"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                {/* Agreement Checkbox */}
+                <div className="flex gap-x-4 sm:col-span-2 items-center">
+                    <div className="relative flex-shrink-0 w-6 h-6 mr-2 transition duration-200 ease-in">
+                        <input type="checkbox" id="toggle" name="toggle" className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 cursor-pointer" checked={isChecked} onChange={handleToggle} />
+                        <label htmlFor="toggle"></label>
+                    </div>
+                    <label className="text-sm leading-6 text-gray-600">
+                        By selecting this, you agree to our
+                        <a href="#" className="font-semibold text-indigo-600"> privacy policy </a>
+                        and
+                        <a href="#" className="font-semibold text-indigo-600"> Terms Condition</a>.
+                    </label>
+                </div>
+
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign up
+                  </button>
+                </div>
+                <div className="mt-4 flex items-center justify-center space-x-4">
+                  {/* Sign in with Google */}
+                  <button
+                    type="button"
+                    className="flex w-full justify-center rounded-md bg-red-600 px-4 py-1.5 text-sm font-semibold  text-white shadow-sm hover:bg-red-500 "
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 mr-2" viewBox="0 0 16 16" fill="currentColor">
+                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
+                    </svg>
+                    Sign in with Google
+                  </button>
+
+                  {/* Sign in with Apple */}
+                  <button
+                    type="button"
+                    className="flex w-full justify-center rounded-md bg-black px-4 py-1.5 text-sm font-semibold  text-white shadow-sm hover:bg-gray-900 "
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 mr-2" viewBox="0 0 16 16" fill="currentColor">
+                    <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
+  <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
+                    </svg>
+                    Sign in with Apple
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Already a member?{' '}
+                <a href="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                  Sign in now
+                </a>
+              </p>
+            </div>
+          
+        </div>
+      </section>
+    </main>
+  )
+}
